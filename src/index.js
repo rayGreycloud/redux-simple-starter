d6a414f1c74d9ff8540866a074e24160eb7bcb01
rayGreycloud <rayGreycloud@gmail.com>
2016-07-01 11:28:32 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = function() {
-  return <div > Hi! < /div>;
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCIa36jKRWEy0mjFfcVUw-mSrvD0VO-Jpg';
+
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
-ReactDOM.render(App);
+ReactDOM.render(<App/> , document.querySelector('.container'));
